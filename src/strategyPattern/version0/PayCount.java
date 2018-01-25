810c17a9b1bdc0c2f2bf0d206ed1296a23553c23
jal <jal@webturing.com>
2018-01-25 17:15:21 +0800

    4

--- src/strategyPattern/version0/PayCount.java
+++ src/strategyPattern/version0/PayCount.java
@@ -9,7 +9,7 @@ import java.awt.event.ActionListener;
  * Created by jal on 2018/1/24 0024.
  */
 public class PayCount extends JFrame implements ActionListener{
-    double total = 0.0d;
+    double price1,number1,total = 0;
     JLabel priceText = new JLabel("price");
     JLabel numberText = new JLabel("number");
     JTextField price = new JTextField(50);
@@ -17,12 +17,15 @@ public class PayCount extends JFrame implements ActionListener{
 
     JButton sure = new JButton("sure");
     JButton reset = new JButton("reset");
-
+    JTextArea jTextArea = new JTextArea(10,40);
+    JLabel show = new JLabel("0.00");
 
     PayCount(){
         super("SupermarketPayCount");
         this.setLayout(new BorderLayout());
+        this.setSize(200,300);
         JPanel jPanel = new JPanel(new GridLayout(2,3));
+
         jPanel.add(priceText);
         jPanel.add(price);
         price.addActionListener(this);
@@ -33,21 +36,29 @@ public class PayCount extends JFrame implements ActionListener{
         number.addActionListener(this);
         jPanel.add(reset);
         reset.addActionListener(this);
-
+        this.add(jPanel,BorderLayout.NORTH);
+        this.add(jTextArea,BorderLayout.CENTER);
+        this.add(show,BorderLayout.SOUTH);
+        //this.pack();
         setVisible(true);
     }
-
-
-    private void btnOK_Click(){
-
-    }
-
     public static void main(String[] args) {
         new PayCount();
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(e.getSource() == sure){
+            price1 =Double.parseDouble( price.getText());
+            number1 = Double.parseDouble(number.getText());
+            total += price1 * number1;
+            show.setText(String.format("%.2f",total));
+            jTextArea.append(String.format("%.2f * %.2f = %.2f\n",price1,number1,total));
+        }else if (e.getSource() == reset){
+            price.setText("");
+            number.setText("");
+            total = 0;
+            jTextArea.setText("");
+            show.setText("0.00");
+        }
     }
 }
